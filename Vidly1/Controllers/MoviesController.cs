045d66478a64cdfe51c29e0501633baed8d1ad53
Seonyx <info@seonyx.com>
2018-01-06 18:07:50 +0100

    Initial upload

---
+++ Vidly1/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+using Vidly1.ViewModels;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "customer 1"},
+                new Customer {Name = "customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
